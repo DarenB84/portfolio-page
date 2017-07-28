4c8eae9c976d7a70fe542264d12a2e2d73027362
DarenB84 <darenbraithwaite84@gmail.com>
2017-07-28 15:34:22 -0400

    initialize repository

---
+++ portfolio.py
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Daren Braithwaite</title>
+  <link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body1>
+  <h1> Daren Braithwaite Portfolio</h1>
+  <p> Paragraph1.<p>
+  <p> Paragraph2.<p>
+<img src="http://rutravel.net/uploads/posts/2015-03/1427748659_16727268647_8d9807626c_o.jpg" alt="http://rutravel.net/538-ulica-pyataya-avenyu.html/" width="1200" height="600">
+</body1>
+ <body2>
+</html>
+<img src="https://cdn.vox-cdn.com/uploads/chorus_image/image/54008287/shutterstock_270402566.0.0.0.jpg" alt="bkpic:" width="1000" height="700">
+<img src="http://www.planetware.com/photos-large/BAR/barbados-bathsheba.jpg"alt="bimpic:" width="800" height="600"
+<h2>This is heading 2</h2>
+<p> Paragraph3.<p>
+</body2>
+</html>
